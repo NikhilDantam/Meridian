66c3ce8257c753f85f2be93e71b0d68cb29ffbc8
Dantam Nikhil <25211a0458@bvrit.ac.in>
2026-08-08 00:11:57 +0530

    Create HomePage component with layout and sections

---
+++ Meridian/src/app/page.tsx
@@ -0,0 +1,22 @@
+import AppLayout from '@/components/AppLayout';
+import HeroBanner from './components/HeroBanner';
+import OrchestratorChat from './components/OrchestratorChat';
+import AgentGrid from './components/AgentGrid';
+import CampusStats from './components/CampusStats';
+import NotificationTicker from './components/NotificationTicker';
+
+export default function HomePage() {
+  return (
+    <AppLayout activePath="/">
+      <div className="min-h-full">
+        <NotificationTicker />
+        <div className="px-4 sm:px-6 lg:px-8 xl:px-10 2xl:px-16 py-6 max-w-screen-2xl mx-auto space-y-8">
+          <HeroBanner />
+          <OrchestratorChat />
+          <CampusStats />
+          <AgentGrid />
+        </div>
+      </div>
+    </AppLayout>
+  );
+}
