c53698e3a6794510ca444ed9e15606813235c32a
Dantam Nikhil <25211a0458@bvrit.ac.in>
2026-08-08 00:03:58 +0530

    Fix import case for UserProfileContent component
    Updated import path for UserProfileContent component.

--- Meridian/src/app/profile/page.tsx
+++ Meridian/src/app/profile/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import AppLayout from '@/components/AppLayout';
-import UserProfileContent from './components/UserProfileContent';
+import UserProfileContent from './Components/UserProfileContent';
 
 export default function ProfilePage() {
   return (
